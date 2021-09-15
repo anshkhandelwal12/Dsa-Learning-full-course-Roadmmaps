049cae9bfc318040079d8013abcc54b9d1ddea0c
Ansh Khandelwal <59608260+anshkhandelwal12@users.noreply.github.com>
2021-09-15 18:23:53 +0530

    Create Celiing.java

---
+++ Arrays/Celiing.java
@@ -0,0 +1,26 @@
+public static int ceiling(int[] arr , int target)
+    {
+        int start = 0;
+        int end = arr.length-1;
+        if(target > arr.length -1)
+        {
+            return -1;
+        }
+        while (start <= end)
+        {
+            int mid = start + (end - start / 2);
+            if(target < arr[mid])
+            {
+                end = mid - 1;
+            }
+            else if(target > arr[mid])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return start;
+        
