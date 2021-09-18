de2b179b1cf82cf4df733e3cb9e55fd5e94b3c31
Ansh Khandelwal <59608260+anshkhandelwal12@users.noreply.github.com>
2021-09-18 21:54:46 +0530

    Update p1.cpp

--- Recursion/p1.cpp
+++ Recursion/p1.cpp
@@ -1,19 +1,28 @@
-#include <bits//stdc++.h>
-using namespace std;
-int main()
-{
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i = 0 ; i < n ; i++)
-    {
-        cin>>arr[i];
+public class Main4{
+    public static void main(String[] args) {
+          int arr[] = {1,2,3,4,5,6};
+          int target = 5;
+        System.out.println(InsertSearch(arr , target ,  0  , arr.length-1));
     }
 
-    int sum = 0;
-    for(int i = 0 ; i < n ; i++)
+    public static int InsertSearch(int[] arr , int target , int s , int e)
     {
-        sum = sum + arr[i];
+//        And s > e then we get that numbers that are s > e and then s is greater than  return-1
+        if(s > e)
+        {
+            return -1;
+        }
+       int m = s + (e - s) / 2;
+//        Recurrence relation in python numbers and codes and learning and
+       if(arr[m] == target)
+       {
+           return m;
+       }
+
+       if(target < m)
+       {
+           return InsertSearch(arr , target , 0 , m-1);
+       }
+       return InsertSearch(arr, target , m+1 , e);
     }
-    cout<<sum<<endl;
 }
